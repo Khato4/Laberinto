e1f30b284c4637df7f4571ebf08e835f82c2da77
Khato4 <khatoiv@gmail.com>
2016-07-03 11:31:01 +0200

    FIN + excepciones propias, final completo

--- src/laberinto/FicheroIn.java
+++ src/laberinto/FicheroIn.java
@@ -19,21 +19,35 @@ public class FicheroIn {
     public FicheroIn(String lab) throws FileNotFoundException {
         this.fr = new FileReader(lab);
         this.br = new BufferedReader(fr);
+        
     }
 
     //mediante la funcion readLine leemos las 2 primeras lineas del fichero y 
     //las guardamos en un array de 2 enteros, uno para fila y otro para columna
-    public int[] getFilasColumnas() throws IOException {
+    public int[] getFilasColumnas() {
         int[] filCol = new int[2];
-        filCol[0] = Integer.parseInt(br.readLine());
-        filCol[1] = Integer.parseInt(br.readLine());
+        try {
+            try {
+                filCol[0] = Integer.parseInt(br.readLine());
+            } catch (IOException ex) {
+                System.out.println("¡Error en la lectura del número de filas!");
+            }
+            filCol[1] = Integer.parseInt(br.readLine());
 
+        } catch (IOException ex) {
+            System.out.println("¡Error en la lectura del número de columnas!");
+        }
         return filCol;
     }
 
     //método para la lectura de cada uno de los bits que definen cada Casilla
-    public int getBit() throws IOException {
-        int bit = br.read() - 48; //convertimos a entero
+    public int getBit() {
+        int bit = 0;
+        try {
+            bit = br.read() - 48; //convertimos a entero
+        } catch (IOException ex) {
+            System.out.println("¡Error en la lectura de bits!");
+        }
         return bit;
 
     }
